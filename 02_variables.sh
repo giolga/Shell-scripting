892df96f47df94a1cc886e451c3adbb36b5f401f
giolga <giolga.kumi@gmail.com>
2025-03-14 12:27:07 -0400

    First Commit

---
+++ 02_variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Yo soy El Kumi"
+name="Kumi"
+age=20
+echo $name, $age
+
+pointer=155.5
+echo "My pointer is:" $pointer
+echo "First envnt varibale:"$SHELL
+
+NOW=$(date '+%F_%H:%M:%S')
+echo "Time:"$NOW
